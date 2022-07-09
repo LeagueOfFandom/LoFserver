a26eb9b8b69d564b53b76f8792759f743f337155
imygnam <imygnam@gmail.com>
2022-07-10 00:15:15 +0900

    [FIX] application

--- src/main/java/com/lofserver/soma/SomaApplication.java
+++ src/main/java/com/lofserver/soma/SomaApplication.java
@@ -1,12 +1,7 @@
 package com.lofserver.soma;
 
-import com.amazonaws.services.secretsmanager.AWSSecretsManager;
-import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
-import com.amazonaws.services.secretsmanager.model.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Base64;
 
 @SpringBootApplication
 public class SomaApplication {
