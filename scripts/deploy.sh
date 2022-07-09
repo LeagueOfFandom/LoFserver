79fee62c2cba140c920ecc93bfba20b9c6c96699
imygnam <imygnam@gmail.com>
2022-07-10 01:46:44 +0900

    [FIX] sh nohup.out fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,4 +17,4 @@ echo "> JAR NAME: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
-nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup /opt/jdk-17/bin/java -jar $JAR_NAME &
