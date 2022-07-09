227ee77c1918e52fde0133d1ad9b740009c87839
imygnam <imygnam@gmail.com>
2022-07-10 00:13:15 +0900

    [FEAT] appspec and deploy

--- src/main/java/com/lofserver/soma/SomaApplication.java
+++ src/main/java/com/lofserver/soma/SomaApplication.java
@@ -1,7 +1,12 @@
 package com.lofserver.soma;
 
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Base64;
 
 @SpringBootApplication
 public class SomaApplication {
@@ -9,5 +14,4 @@ public class SomaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SomaApplication.class, args);
 	}
-	// git develop
 }
