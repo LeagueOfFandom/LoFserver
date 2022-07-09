7b47af0dd614649d6d6f21bcdcc1ea4f9df71378
imygnam <imygnam@gmail.com>
2022-07-10 01:22:34 +0900

    [FIX] sh id add

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 REPOSITORY=/home/app
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fla java)
+CURRENT_PID=$(pgrep -fla java | awk '{print $1}')
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
