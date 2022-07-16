5a6f7549916870c3ff57fd1770e3c2e74a632c75
summit45 <brandy2145@gmail.com>
2022-07-17 00:31:51 +0900

    [FEAT] crawl test

--- src/main/java/com/lofserver/soma/entity/match/MatchInfo.java
+++ src/main/java/com/lofserver/soma/entity/match/MatchInfo.java
@@ -1,11 +1,13 @@
 package com.lofserver.soma.entity.match;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 @Getter
+@Setter
 public class MatchInfo {
     private LocalDate matchDate;
     private LocalTime matchTime;
