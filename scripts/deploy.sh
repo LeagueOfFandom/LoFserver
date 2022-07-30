8aa296e854a0bdd2b5e29f591656e52b6392b905
summit45 <81628744+summit45@users.noreply.github.com>
2022-07-30 22:05:09 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,4 +18,4 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 echo "> jasypt_password ${JASYPT_PASSWORD}"
-nohup /opt/jdk-17/bin/java -javaagent:/home/app/dd-java-agent.jar -Djasypt.encryptor.password=${JASYPT_PASSWORD} -Ddd.logs.injection=true -Ddd.service=my-app -Ddd.env=staging -jar $JAR_NAME 1>$REPOSITORY/lof.log 2>&1 &
+nohup /opt/jdk-17/bin/java -Djasypt.encryptor.password=${JASYPT_PASSWORD} -jar $JAR_NAME 1>$REPOSITORY/lof.log 2>&1 &
