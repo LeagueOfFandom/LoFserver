616490d9564b7fe6aaa37e8efb2beae1e9e3cba5
imygnam <imygnam@gmail.com>
2022-08-11 07:02:59 +0900

    [FIX] api

--- src/main/java/com/lofserver/soma/controller/v1/DataSetController.java
+++ src/main/java/com/lofserver/soma/controller/v1/DataSetController.java
@@ -10,11 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class DataSetController {
 
