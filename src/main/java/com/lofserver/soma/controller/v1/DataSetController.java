4f27a03671e3be76ee72c53f1039da256bce97d5
imygnam <imygnam@gmail.com>
2022-08-11 07:23:22 +0900

    [FIX] localdate

--- src/main/java/com/lofserver/soma/controller/v1/DataSetController.java
+++ src/main/java/com/lofserver/soma/controller/v1/DataSetController.java
@@ -6,6 +6,7 @@ import com.lofserver.soma.repository.MatchDetailsRepository;
 import com.lofserver.soma.repository.MatchRepository;
 import com.lofserver.soma.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +18,14 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class DataSetController {
 
     private MatchDetailsRepository matchDetailsRepository;
     private MatchRepository matchRepository;
     @PostMapping("/dataset")
-    public String dataset(@RequestParam(value = "homeTeamid")Long homeTeamId, @RequestParam(value = "awayTeamid")Long awayTeamId, @RequestParam(value = "date") LocalDate localDate, @RequestBody List<MatchDetailSet> matchDetailSetList) {
+    public String dataset(@RequestParam(value = "homeTeamid")Long homeTeamId, @RequestParam(value = "awayTeamid")Long awayTeamId, @RequestParam(value = "date") String localDate1, @RequestBody List<MatchDetailSet> matchDetailSetList) {
+        LocalDate localDate = LocalDate.parse(localDate1);
         matchDetailsRepository.save(new MatchDetailsEntity(matchRepository.findByTeamIdsAndMatchDate(homeTeamId, awayTeamId, localDate.getYear(),localDate.getMonthValue(),localDate.getDayOfMonth()).getMatchId(),matchDetailSetList));
         return "success";
     }
