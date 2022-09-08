b4c79d65cf8c1207ad14f47e5fcba9732628f5ad
imygnam <imygnam@gmail.com>
2022-09-09 03:01:57 +0900

    변경된 entity 수정.

--- src/main/java/com/lofserver/soma/entity/LeagueEntity.java
+++ src/main/java/com/lofserver/soma/entity/LeagueEntity.java
@@ -1,7 +1,7 @@
 package com.lofserver.soma.entity;
 
-import com.lofserver.soma.dto.crawlDto.gameDto.sub.player.PlayerDetails;
-import com.lofserver.soma.dto.teamsDetailDto.sub.Status;
+import com.lofserver.soma.dto.leagueDto.Series;
+import com.lofserver.soma.dto.leagueDto.VideoGame;
 import com.vladmihalcea.hibernate.type.json.JsonStringType;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,40 +12,42 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@Table(name = "team_list")
+@Table(name = "league")
 @TypeDef(name = "json", typeClass = JsonStringType.class)
-public class TeamEntity {
+public class LeagueEntity {
     @Id
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "acronym")
-    private String acronym;
-
     @Column(name = "image_url")
     private String image_url;
 
-    @Column(name = "location")
-    private String location;
+    @Column(name = "modified_at")
+    private LocalDateTime modified_at;
 
     @Column(name = "name")
     private String name;
 
     @Type(type = "json")
-    @Column(name = "players", columnDefinition = "json")
-    private List<PlayerDetails> players;
+    @Column(name = "series", columnDefinition = "json")
+    private List<Series> series;
+
+    @Column(name = "latest_series_id")
+    private Long latest_series_id;
+    @Column(name = "slug")
+    private String slug;
+
+    @Column(name = "url")
+    private String url;
 
     @Type(type = "json")
-    @Column(name = "status", columnDefinition = "json")
-    private Status status;
-
-    @Column(name = "series_id")
-    private Long series_id;
+    @Column(name = "videogame", columnDefinition = "json")
+    private VideoGame videogame;
 
 }
-
