c30b89a3626337dce1a68f1dc028edbe07b33b22
imygnam <imygnam@gmail.com>
2022-09-08 12:58:06 +0900

    [FEAT] JWT token확인 함수 추가 및 test

--- src/main/java/com/lofserver/soma/config/JsonWebToken.java
+++ src/main/java/com/lofserver/soma/config/JsonWebToken.java
@@ -1,9 +1,7 @@
 package com.lofserver.soma.config;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +12,7 @@ import java.time.Duration;
 import java.util.Date;
 
 @Component
+@Slf4j
 public class JsonWebToken {
 
     private final String secretKey;
@@ -29,16 +28,31 @@ public class JsonWebToken {
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // header type (jwt)
                 .setIssuer("lof-server") // (iss setting)
                 .setIssuedAt(now) // (iat setting)
-                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(30).toMillis())) // (exp setting)
+                .setExpiration(new Date(now.getTime() + Duration.ofDays(1).toMillis())) // (exp setting)
                 .claim("id", id) // (secret claim setting)
                 .signWith(SignatureAlgorithm.HS256, secretKey) // (signature setting)
                 .compact();
     }
 
-    public Claims parseJwtToken(String token){
+    public Claims parseJwtToken(String token) {
         return Jwts.parser()
                 .setSigningKey(secretKey)
                 .parseClaimsJws(token)
                 .getBody();
+    }
+
+    public Boolean checkJwtToken(String token){
+        try{
+            Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token);
+        }catch (ExpiredJwtException e){
+            log.info("ExpiredJwtException");
+            return false;
+        }catch (Exception e){
+            log.info("JWTException");
+            return false;
+        }
+        return true;
     }
 }
