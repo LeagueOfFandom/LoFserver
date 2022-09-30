ef39aef31886995450e612438f67c576d3d05e2e
summit45 <brandy2145@gmail.com>
2022-09-30 15:22:40 +0900

    [FIX] 작성/수정 시간 JPA Auditing으로 변경

--- src/main/java/com/lofserver/soma/SomaApplication.java
+++ src/main/java/com/lofserver/soma/SomaApplication.java
@@ -2,9 +2,11 @@ package com.lofserver.soma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SomaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SomaApplication.class, args);
