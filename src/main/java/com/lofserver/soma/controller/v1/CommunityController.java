b4cbb38705278973d18783a0e0c26f65c31abaef
summit45 <brandy2145@gmail.com>
2022-09-30 15:36:01 +0900

    [FEAT] 커뮤니티 주소 수정

--- src/main/java/com/lofserver/soma/controller/v1/CommunityController.java
+++ src/main/java/com/lofserver/soma/controller/v1/CommunityController.java
@@ -25,20 +25,20 @@ public class CommunityController{
 
     //페이징 추가해서 나눠서 주기
     @ApiOperation(value = "게시판 목록 조회 Api", notes = "client에서 GET하면 모든 게시글을 반환한다.")
-    @GetMapping("/board")
+    @GetMapping("/boards")
     public ResponseEntity<?> getBoardList() throws Exception {
         return communityService.getBoardList();
     }
 
     @ApiOperation(value = "게시판 글 작성 Api", notes = "client에서 게시글을 작성한 후 POST하면 server에 저장한다.")
-    @PostMapping(value="/board/write")
-    public ResponseEntity<String> insertBoard(@RequestBody BoardDto boardDto) throws Exception {
+    @PostMapping(value="/boards")
+    public ResponseEntity<?> insertBoard(@RequestBody BoardDto boardDto) throws Exception {
         return communityService.writeBoard(boardDto);
     }
 
     @ApiOperation(value = "게시글 상세화면 조회 Api", notes = "client에서 게시글 id를 GET하면 해당 게시글을 반환한다.")
-    @GetMapping(value="/board/detail")
-    public ResponseEntity<?> getBoardDetail(@RequestParam(value="boardId")Long boardId) throws Exception {
+    @GetMapping(value="/boards/{id}")
+    public ResponseEntity<?> getBoardDetail(@PathVariable("id") Long boardId)throws Exception {
         return communityService.getBoardDetail(boardId);
     }
 
