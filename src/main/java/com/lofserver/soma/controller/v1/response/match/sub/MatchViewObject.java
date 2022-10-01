0988fbd1ff0ae23ad7a0f7edfdd5ee5e0a8fff81
imygnam <imygnam@gmail.com>
2022-10-01 14:39:41 +0900

    시간을 날짜와 time으로 쪼개기 적용

--- src/main/java/com/lofserver/soma/controller/v1/response/match/sub/MatchViewObject.java
+++ src/main/java/com/lofserver/soma/controller/v1/response/match/sub/MatchViewObject.java
@@ -4,6 +4,8 @@ import com.lofserver.soma.dto.pandaScoreDto.matchDto.sub.Opponent;
 import com.lofserver.soma.entity.MatchEntity;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MatchViewObject {
     private Long matchId;
@@ -11,6 +13,7 @@ public class MatchViewObject {
     private String homeImg;
     private String awayName;
     private String awayImg;
+    private String date;
     private String time;
     private String league;
     private Boolean isAlarm;
@@ -20,12 +23,15 @@ public class MatchViewObject {
     private String status;
 
     public MatchViewObject(MatchEntity matchEntity, Boolean isAlarm){
+        LocalDateTime scheduledAt = matchEntity.getBeginAt();
+        scheduledAt.plusHours(9);
         this.matchId = matchEntity.getId();
         this.homeName = matchEntity.getOpponents().get(0).getOpponent().getAcronym();
         this.homeImg = matchEntity.getOpponents().get(0).getOpponent().getImage_url();
         this.awayName = matchEntity.getOpponents().get(1).getOpponent().getAcronym();
         this.awayImg = matchEntity.getOpponents().get(1).getOpponent().getImage_url();
-        this.time = matchEntity.getBeginAt().toString();
+        this.date = scheduledAt.toLocalDate().toString();
+        this.time = scheduledAt.toLocalTime().toString();
         this.league = matchEntity.getTournament().getName();
         this.isAlarm = isAlarm;
         this.homeScore = matchEntity.getResults().get(0).getScore();
